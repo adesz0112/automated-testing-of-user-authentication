9a5b2dde07b2210a6b5f619578800f5068f6f0d3
adesz0112 <meszaros.adam94@gmail.com>
2025-06-27 15:31:01 +0200

    Created the functions for registration

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,4 +1,31 @@
 package pages;
 
-public class LoginPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends BasePage {
+
+    @FindBy(css = "input[data-qa='signup-name']")
+    private WebElement nameInput;
+    @FindBy(css = "input[data-qa='signup-email']")
+    private WebElement emailInput;
+    @FindBy(css = "button[data-qa='signup-button']")
+    private WebElement signUpButton;
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void enterNameForSignUp(String name) {
+        typeInto(nameInput, name);
+    }
+
+    public void enterEmailForSignUp(String email) {
+        typeInto(emailInput, email);
+    }
+
+    public void clickSignUpButton() {
+        clickOn(signUpButton);
+    }
 }
--- src/main/java/pages/RegistrationPage.java
+++ src/main/java/pages/RegistrationPage.java
@@ -1,4 +1,116 @@
 package pages;
 
-public class RegistrationPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
+
+public class RegistrationPage extends BasePage {
+    @FindBy(id = "id_gender1")
+    private WebElement mrRadioButton;
+    @FindBy(id = "password")
+    private WebElement passwordInput;
+    @FindBy(id = "days")
+    private WebElement dayDropdown;
+    @FindBy(id = "months")
+    private WebElement monthDropDown;
+    @FindBy(id = "years")
+    private WebElement yearDropDown;
+    @FindBy(id = "first_name")
+    private WebElement firstNameInput;
+    @FindBy(id = "last_name")
+    private WebElement lastNameInput;
+    @FindBy(id = "company")
+    private WebElement companyInput;
+    @FindBy(id = "address1")
+    private WebElement address1Input;
+    @FindBy(id = "address2")
+    private WebElement address2Input;
+    @FindBy(id = "country")
+    private WebElement countryDropDown;
+    @FindBy(id = "state")
+    private WebElement stateInput;
+    @FindBy(id = "city")
+    private WebElement cityInput;
+    @FindBy(id = "zipcode")
+    private WebElement zipcodeInput;
+    @FindBy(id = "mobile_number")
+    private WebElement mobileNumberInput;
+
+
+
+    public RegistrationPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void selectTitleMr() {
+        clickOn(mrRadioButton);
+    }
+
+    public void selectTitle(String title) {
+        String genderId = title.equalsIgnoreCase("Mr") ? "id_gender1" : "id_gender2";
+        WebElement genderRadio = driver.findElement(By.id(genderId));
+        clickOn(genderRadio);
+    }
+
+    public void enterPassword(String password) {
+        typeInto(passwordInput, password);
+    }
+
+    public void selectDayByValue(String value) {
+        Select select = new Select(dayDropdown);
+        select.selectByValue(value);
+    }
+
+    public void selectMonthByValue(String value) {
+        Select select = new Select(monthDropDown);
+        select.selectByValue(value);
+    }
+
+    public void selectYearByValue(String value) {
+        Select select = new Select(yearDropDown);
+        select.selectByValue(value);
+    }
+
+    public void enterFirstName(String name) {
+        typeInto(firstNameInput, name);
+    }
+
+    public void enterLastName(String name) {
+        typeInto(lastNameInput, name);
+    }
+
+    public void enterCompany(String company) {
+        typeInto(companyInput, company);
+    }
+
+    public void enterAddress1(String address1) {
+        typeInto(address1Input, address1);
+    }
+
+    public void enterAddress2(String address2) {
+        typeInto(address2Input, address2);
+    }
+
+    public void enterCountry(String country) {
+        Select select = new Select(countryDropDown);
+        select.selectByValue(country);
+    }
+
+    public void enterState(String state) {
+        typeInto(stateInput, state);
+    }
+
+    public void enterCity(String city) {
+        typeInto(cityInput, city);
+    }
+
+    public void enterZipcode(String zipcode) {
+        typeInto(zipcodeInput, zipcode);
+    }
+
+    public void enterMobileNumber(String mobileNumber) {
+        typeInto(mobileNumberInput, mobileNumber);
+    }
 }
